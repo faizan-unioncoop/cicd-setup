76a8a49b835ee579f4da60ad26c1b20029d0d427
faizan-unioncoop <116625446+faizan-unioncoop@users.noreply.github.com>
2023-02-01 10:11:44 +0400

    remove pods

--- Pods/Target Support Files/Pods-cicd-setup/Pods-cicd-setup-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cicd_setup : NSObject
-@end
-@implementation PodsDummy_Pods_cicd_setup
-@end
--- Pods/Target Support Files/Pods-cicd-setupTests/Pods-cicd-setupTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cicd_setupTests : NSObject
-@end
-@implementation PodsDummy_Pods_cicd_setupTests
-@end
